7ce0fe0a18b41314055a7f03b3b04c08342b6478
BogChern <bogdan.cherniy2002@gmail.com>
2022-01-18 19:54:45 +0400

    Выгрузка информации

---
+++ ChequeEmpty.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class ChequeEmpty : Form
+    {
+        public ChequeEmpty()
+        {
+            InitializeComponent();
+        }
+
+        private void ChequeEmpty_Load(object sender, EventArgs e)
+        {
+            label1.Text = $"Ошибка! Корзина пуста.";
+            button1.Text = "OK";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ChequeEmpty_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
