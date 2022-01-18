2f7d72da2977c9b2954d92ed99c758350e61006d
BogChern <bogdan.cherniy2002@gmail.com>
2022-01-18 20:04:21 +0400

    Реализован ввод размеров помещения

---
+++ ProductIsNotSelected.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class ProductIsNotSelected : Form
+    {
+        public ProductIsNotSelected()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductIsNotSelected_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Ошибка! Товар не выбран.";
+            button1.Text = "Ok";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
