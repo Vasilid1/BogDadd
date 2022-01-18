d7363dbbc30d03ca50cfd8e1b80677df951cb61d
BogChern <bogdan.cherniy2002@gmail.com>
2022-01-18 19:55:44 +0400

    Реализована возможность добавления информации в корзину

---
+++ IncorrectData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class IncorrectData : Form
+    {
+        public IncorrectData()
+        {
+            InitializeComponent();
+        }
+
+        private void IncorrectData_Load(object sender, EventArgs e)
+        {
+            label1.Text = $"Ошибка! Введены некорректные данные.";
+            button1.Text = "OK";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
